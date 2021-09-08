7dfad47a8f75fdd0be4bb316d898391361606826
Kaunain <kaunaink@gmail.com>
2021-09-07 23:34:06 -0400

    updated mongo credentials and url

--- server/settings.js
+++ server/settings.js
@@ -5,6 +5,7 @@ dotenv.config();
 // const mongoUrl = "mongodb://localhost:27017/userDB";
 
 // connect to mongoDB Atlas
-const mongoUrl = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.xwdfx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const mongoUrl = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster1.xkb3y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+`;
 
 export default mongoUrl;
